06e7d16a8ddd3e85aa286ca9617b0dd82960aa86
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2023-07-04 12:03:19 +0100

    removed

--- apps/chrome/src/background/master.ts
+++
@@ -1,9 +0,0 @@
-import type { PlasmoMessaging } from "@plasmohq/messaging";
-
-const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
-    res.send({
-        status: 200
-    })
-}
-
-export default handler
