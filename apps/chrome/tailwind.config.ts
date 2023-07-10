0fb26dfb2330e633bffb54a51bf7bf7603727747
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2023-07-10 15:57:11 +0100

    fixed tailwind config

--- apps/chrome/tailwind.config.ts
+++ apps/chrome/tailwind.config.ts
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+
+import baseConfig from "@acme/tailwind-config";
+
 module.exports = {
   content: ["./**/*.tsx"],
+  presets: [baseConfig],
   theme: {
     extend: {},
   },
