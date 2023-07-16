a3dd744aaba963f0f4d2971dbe187770f1dec52c
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2023-07-16 19:52:37 +0100

    update to the demos

--- apps/chrome/src/contents/demo.ts
+++ apps/chrome/src/contents/demo.ts
@@ -1,3 +1,6 @@
+import { Auction, selectAuction } from "~slices/auction-slice";
+import { Platform, selectPlatform } from "~slices/platform-slice";
+import { persister, store } from "~store";
 import { PlasmoCSConfig } from "plasmo";
 
 export const config: PlasmoCSConfig = {
@@ -6,12 +9,35 @@ export const config: PlasmoCSConfig = {
   run_at: "document_start",
 };
 
-const counter = document.getElementById("count");
+const auctionState: Auction = selectAuction(store.getState());
+const platformState: Platform[] = selectPlatform(store.getState());
 
-const delay = (time: number) =>
-  new Promise((resolve) => setTimeout(resolve, time));
+persister.subscribe(() => {
+  auctionState.lots.forEach((lot) => {
+    console.log(lot);
+    alert("hi");
+  });
+});
 
-while (true) {
-  await delay(1000);
-  counter.innerText = String(parseInt(counter.innerText) + 1);
-}
+addEventListener("DOMContentLoaded", () => {
+  const counter = document.getElementById("count");
+
+  const delay = (time: number) =>
+    new Promise((resolve) => setTimeout(resolve, time));
+
+  const func = async () => {
+    await delay(1000);
+    console.log("hi");
+    counter.innerText = String(parseInt(counter.innerText) + 1);
+  };
+
+  const click = () => {
+    console.log("clicked");
+    counter.click();
+  };
+
+  store.subscribe(() => {
+    console.log("store updated");
+    click();
+  });
+});
--- apps/nextjs/src/pages/demo.tsx
+++ apps/nextjs/src/pages/demo.tsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [word, setWord] = useState("hello");
   const increment = () => setCount(count + 1);
   return (
-    <button id="count" onClick={increment}>
-      {count}
-    </button>
+    <div>
+      <button id="count" onClick={increment}>
+        {count}
+      </button>
+      <input id="word" value={word} onChange={(e) => setWord(e.target.value)} />
+      <h3>{word}</h3>
+    </div>
   );
 };
 
