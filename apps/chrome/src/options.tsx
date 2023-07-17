6249db35c22b3f1573c69bf273d5a92af5cb5988
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2023-07-17 16:54:40 +0100

    built options page

--- apps/chrome/src/options.tsx
+++ apps/chrome/src/options.tsx
@@ -5,6 +5,7 @@
 import exp from "constants";
 import { useEffect, useState } from "react";
 import {
+  createLot,
   resetState,
   setAuctionDate,
   setAuctionName,
@@ -19,6 +20,8 @@ const Options = () => {
   return (
     <Wrapper>
       <LotsTable />
+      <CreateLot />
+      <SetCurrentLot />
     </Wrapper>
   );
 };
@@ -33,25 +36,114 @@ const LotsTable = () => {
       <thead>
         <tr>
           <th>Lot Number</th>
-          <th>Lot Name</th>
           <th>Lot Description</th>
-          <th>Lot Estimate</th>
-          <th>Lot Price</th>
+          <th>Lot Low Estimate</th>
+          <th>Lot High Estimate</th>
+          <th>Lot Asking</th>
+          <th>Lot State</th>
         </tr>
       </thead>
       <tbody>
         {lots.map((lot) => (
-          <tr key={lot.id.toString() + lot.name}>
+          <tr key={lot.id.toString() + lot.description}>
             <td>{lot.id}</td>
-            <td>{lot.name}</td>
             <td>{lot.description}</td>
             <td>{lot.lowEstimate}</td>
             <td>{lot.highEstimate}</td>
+            <td>{lot.asking}</td>
             <td>{lot.state}</td>
-            <td>{lot.bids.length}</td>
           </tr>
         ))}
       </tbody>
     </table>
+  );
+};
+
+const CreateLot = () => {
+  // create a rfc which is used to create a lot
+  const dispatch = useAppDispatch();
+  const [lotNumber, setLotNumber] = useState("");
+  const [lotDescription, setLotDescription] = useState("");
+  const [lotLowEstimate, setLotLowEstimate] = useState(0);
+  const [lotHighEstimate, setLotHighEstimate] = useState(0);
+  const [lotAsking, setLotAsking] = useState(0);
+  const [lotState, setLotState] = useState("unsold");
+  const lotBids = [];
+  const image = "";
+
+  const createLotDispatch = () => {
+    dispatch(
+      createLot({
+        id: lotNumber,
+        description: lotDescription,
+        lowEstimate: lotLowEstimate,
+        highEstimate: lotHighEstimate,
+        asking: lotAsking,
+        state: lotState,
+        bids: lotBids,
+        image: image,
+      }),
+    );
+  };
+
+  return (
+    <div>
+      <h1>Create Lot</h1>
+      <input
+        type="text"
+        placeholder="Lot Number"
+        value={lotNumber}
+        onChange={(e) => setLotNumber(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Lot Description"
+        value={lotDescription}
+        onChange={(e) => setLotDescription(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Lot Low Estimate"
+        value={lotLowEstimate}
+        onChange={(e) => setLotLowEstimate(parseInt(e.target.value))}
+      />
+      <input
+        type="number"
+        placeholder="Lot High Estimate"
+        value={lotHighEstimate}
+        onChange={(e) => setLotHighEstimate(parseInt(e.target.value))}
+      />
+      <input
+        type="number"
+        placeholder="Lot Asking"
+        value={lotAsking}
+        onChange={(e) => setLotAsking(parseInt(e.target.value))}
+      />
+      <select value={lotState} onChange={(e) => setLotState(e.target.value)}>
+        <option value="unsold">Unsold</option>
+        <option value="sold">Sold</option>
+      </select>
+      <button onClick={createLotDispatch}>Create Lot</button>
+    </div>
+  );
+};
+
+const SetCurrentLot = () => {
+  const auction: Auction = useAppSelector((state: RootState) => state.auction);
+  const [lotNumber, setLotNumberState] = useState(auction.currentLotId);
+  const dispatch = useAppDispatch();
+  return (
+    <div>
+      <h1>Set Current Lot</h1>
+      <input
+        type="text"
+        placeholder="Lot Number"
+        value={lotNumber}
+        onChange={(e) => setLotNumberState(e.target.value)}
+      />
+      <button onClick={() => dispatch(setLotNumber(lotNumber))}>
+        Set Lot Number
+      </button>
+    </div>
   );
 };
