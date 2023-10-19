fd1db794aa9a8f1a1358d3546453c3140aab7a4d
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2023-10-19 13:13:34 +0100

    Fixed possible null items in ELBodge

--- apps/chrome/src/contents/ELBodge.ts
+++ apps/chrome/src/contents/ELBodge.ts
@@ -11,15 +11,19 @@ export const config: PlasmoCSConfig = {
 document.addEventListener("DOMContentLoaded", () => {
   // find the element with the id of launchAuctioneer
   const launchAuctioneer = document.getElementById("launchAuctioneer");
+  if (!launchAuctioneer) return;
 
   // get the contents of the onclick attribute
-  const auctionId = launchAuctioneer.getAttribute("onclick").split("'")[1];
+  const auctionId = launchAuctioneer.getAttribute("onclick")?.split("'")[1];
 
   // Because i can't remove the onClick event listener, i'm and going to create a new button copy the styles from the old button and place it into the new one. I am also going to move the image inside the old button into the new one.
   // create a new button
   const newButton = document.createElement("button");
   // copy the styles from the old button
-  newButton.setAttribute("style", launchAuctioneer?.getAttribute("style"));
+  newButton.setAttribute(
+    "style",
+    launchAuctioneer?.getAttribute("style") || "",
+  );
   // move the image into the new button
   newButton.appendChild(launchAuctioneer.children[0]);
 
