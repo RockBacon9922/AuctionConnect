1943be29c40500fd720ca033527cc944a9eb585f
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2023-10-27 13:56:24 +0100

    added descriptions

--- packages/element-operations/index.ts
+++ packages/element-operations/index.ts
@@ -1,4 +1,12 @@
 // This function can be used to observe changes in the DOM
+
+/**
+ *
+ * @param elementId
+ * @param callback
+ * Function that runs a callback function everytime there is a change to the passed element in the DOM
+ */
+
 export const observeElementByIdContent = (
   elementId: string,
   callback: Function,
@@ -20,12 +28,11 @@ export const observeElementByIdContent = (
   });
 };
 
-// This function can be used to observe changes in the DOM
 /**
+ * @description Function that runs a callback function everytime there is a change to the passed element in the DOM
  *
- * @param element
- * @param callback
- * Function that runs a callback function everytime there is changes to the passed element
+ * @param {Element} element
+ * @param {Function} callback
  */
 export const observeElementContent = (
   element: Element, // The element to be observed
@@ -46,6 +53,11 @@ export const observeElementContent = (
   });
 };
 
+/**
+ *
+ * @param id
+ * @param value
+ */
 export const updateInput = (id: string, value: string) => {
   const input = document.getElementById(id);
   // React >=16
@@ -56,6 +68,6 @@ export const updateInput = (id: string, value: string) => {
   nativeInputValueSetter?.call(input, value);
 
   // i know for a fact that "input" event works for react. "change" event works for blazor and react let's hope that works for everything.
-  var ev = new Event("change", { bubbles: true });
+  var ev = new Event("change", { bubbles: true }); // Bubbles means event propogates across the whole DOM. I think writing this comment without internet.
   input?.dispatchEvent(ev);
 };
