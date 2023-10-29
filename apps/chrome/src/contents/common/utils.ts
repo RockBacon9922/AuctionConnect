ec1bb462a050e2724e7dd680c0a8418dbda5b7fd
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2023-10-29 21:21:33 +0000

    Utils Completed

--- apps/chrome/src/contents/common/utils.ts
+++ apps/chrome/src/contents/common/utils.ts
@@ -1,80 +1,102 @@
+import {
+  createBid,
+  createLot,
+  setActiveLot,
+  type CreateBid,
+} from "~slices/auction-slice";
+import { getState, persister, store } from "~store";
+
 import { updateInput } from "@acme/element-operations";
 
-export const getLot = (consoleElementLotId: string) => {
-  return document
-    .getElementById(consoleElementLotId)
-    ?.innerText.replace("Lot ", "");
+// I need to redo all the functions below but the input argument is a HTMLElement or HTMLInputElement respectively
+
+export const getLot = (currentLot: HTMLElement) => {
+  return currentLot.innerText.replace("Lot ", "");
 };
 
-export const getImage = (consoleLotImageElementId: string) => {
+export const getImage = (lotImage: HTMLImageElement) => {
   // get image url
-  const image = document.getElementById(
-    consoleLotImageElementId,
-  ) as HTMLImageElement;
-  return image.src;
+  return lotImage.src;
 };
 
-export const getAsk = (consoleElementAskId: string) => {
-  return document
-    .getElementById(consoleElementAskId)
-    ?.innerText.replace("Asking: ", "")
-    .replace(",", "");
+export const getAsk = (ask: HTMLElement) => {
+  return ask.innerText.replace("Asking: ", "").replace(",", "");
 };
 
-export const getHammer = (consoleElementHammerId: string) => {
-  return document
-    .getElementById(consoleElementHammerId)
-    ?.innerText.replace("Bid: ", "")
-    .replace(",", "");
+export const getHammer = (hammer: HTMLElement) => {
+  return hammer.innerText.replace("Bid: ", "").replace(",", "");
 };
 
-export const getLowEstimate = (consoleElementLowEstimateId: string) => {
-  return document
-    .getElementById(consoleElementLowEstimateId)
-    ?.innerText.replace("Low Estimate: ", "")
-    .replace(",", "");
+export const getLowEstimate = (lowEstimate: HTMLElement) => {
+  return lowEstimate.innerText
+    .replace("Est: ", "")
+    .replaceAll("£", "")
+    .split(" - ")[0];
 };
 
-export const getHighEstimate = (consoleElementHighEstimateId: string) => {
-  return document
-    .getElementById(consoleElementHighEstimateId)
-    ?.innerText.replace("High Estimate: ", "")
-    .replace(",", "");
+export const getHighEstimate = (highEstimate: HTMLElement) => {
+  return highEstimate.innerText
+    .replace("Est: ", "")
+    .replaceAll("£", "")
+    .split(" - ")[1];
 };
 
-export const getDescription = (consoleElementDesriptionId: string) => {
-  return document
-    .getElementById(consoleElementDesriptionId)
-    ?.innerText.replace("description: ", "");
+export const getDescription = (description: HTMLElement) => {
+  return description.innerText.replace("description: ", "");
 };
 
-export const getBidder = (consoleElementCurrentBidderId: string) => {
-  return document
-    .getElementById(consoleElementCurrentBidderId)
-    ?.innerText.replace("Bidder: ", "");
+export const getBidder = (currentBidder: HTMLElement) => {
+  return currentBidder.innerText;
 };
 
-export const setAsk = (consoleElementAskInputId: string, ask: number) => {
-  updateInput(consoleElementAskInputId, ask.toString());
-  document.getElementById(consoleElementAskInputId)?.click();
+export const setAsk = (askInput: HTMLInputElement, ask: number) => {
+  updateInput(askInput, ask.toString());
+  askInput.click();
 };
 
-export const clickBid = (consoleElementBidButtonId: string) => {
-  document.getElementById(consoleElementBidButtonId)?.click();
+export const clickBid = (bidButton: HTMLElement) => {
+  bidButton.click();
 };
 
-export const clickSold = (consoleElementSellButtonId: string) => {
-  document.getElementById(consoleElementSellButtonId)?.click();
+export const clickSold = (sellButton: HTMLElement) => {
+  sellButton.click();
 };
 
-export const clickPass = (consoleElementPassButtonId: string) => {
-  document.getElementById(consoleElementPassButtonId)?.click();
+export const clickPass = (passButton: HTMLElement) => {
+  passButton.click();
 };
 
-export const clickRoom = (consoleElementRoomButtonId: string) => {
-  document.getElementById(consoleElementRoomButtonId)?.click();
+export const clickRoom = (roomButton: HTMLElement) => {
+  roomButton.click();
 };
 
-export const clickNextLot = (consoleElementNextLotButtonId: string) => {
-  document.getElementById(consoleElementNextLotButtonId)?.click();
+export const clickNextLot = (nextLotButton: HTMLElement) => {
+  nextLotButton.click();
+};
+
+const createOrUpdateActiveLot = (
+  lotId: string,
+  asking: number,
+  description: string,
+  highEstimate: number,
+  lowEstimate: number,
+  image: string,
+) => {
+  const lotExists = getState().auction.lots.some((lot) => lot.id === lotId);
+  // if lot does not exist. Create it!!!
+  if (!lotExists) {
+    store.dispatch(
+      createLot({
+        id: lotId,
+        asking: asking,
+        bids: [],
+        description,
+        highEstimate,
+        lowEstimate,
+        image,
+        state: "unsold",
+      }),
+    );
+  }
+  store.dispatch(setActiveLot(lotId));
 };
