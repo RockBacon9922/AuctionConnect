e182cf4c39c263d2d71df53a706b93435226884e
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2023-10-29 21:45:00 +0000

    Realised not all the functions were suitable for cross platform use.

--- apps/chrome/src/contents/common/utils.ts
+++ apps/chrome/src/contents/common/utils.ts
@@ -1,80 +1,14 @@
-import {
-  createBid,
-  createLot,
-  setActiveLot,
-  type CreateBid,
-} from "~slices/auction-slice";
-import { getState, persister, store } from "~store";
+import { createLot, setActiveLot } from "~slices/auction-slice";
+import { getState, store } from "~store";
 
 import { updateInput } from "@acme/element-operations";
-
-// I need to redo all the functions below but the input argument is a HTMLElement or HTMLInputElement respectively
-
-export const getLot = (currentLot: HTMLElement) => {
-  return currentLot.innerText.replace("Lot ", "");
-};
-
-export const getImage = (lotImage: HTMLImageElement) => {
-  // get image url
-  return lotImage.src;
-};
-
-export const getAsk = (ask: HTMLElement) => {
-  return ask.innerText.replace("Asking: ", "").replace(",", "");
-};
-
-export const getHammer = (hammer: HTMLElement) => {
-  return hammer.innerText.replace("Bid: ", "").replace(",", "");
-};
-
-export const getLowEstimate = (lowEstimate: HTMLElement) => {
-  return lowEstimate.innerText
-    .replace("Est: ", "")
-    .replaceAll("£", "")
-    .split(" - ")[0];
-};
-
-export const getHighEstimate = (highEstimate: HTMLElement) => {
-  return highEstimate.innerText
-    .replace("Est: ", "")
-    .replaceAll("£", "")
-    .split(" - ")[1];
-};
-
-export const getDescription = (description: HTMLElement) => {
-  return description.innerText.replace("description: ", "");
-};
-
-export const getBidder = (currentBidder: HTMLElement) => {
-  return currentBidder.innerText;
-};
 
 export const setAsk = (askInput: HTMLInputElement, ask: number) => {
   updateInput(askInput, ask.toString());
   askInput.click();
 };
 
-export const clickBid = (bidButton: HTMLElement) => {
-  bidButton.click();
-};
-
-export const clickSold = (sellButton: HTMLElement) => {
-  sellButton.click();
-};
-
-export const clickPass = (passButton: HTMLElement) => {
-  passButton.click();
-};
-
-export const clickRoom = (roomButton: HTMLElement) => {
-  roomButton.click();
-};
-
-export const clickNextLot = (nextLotButton: HTMLElement) => {
-  nextLotButton.click();
-};
-
-const createOrUpdateActiveLot = (
+export const createOrUpdateActiveLot = (
   lotId: string,
   asking: number,
   description: string,
