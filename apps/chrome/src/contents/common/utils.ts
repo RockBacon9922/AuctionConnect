8557717d8c7d32b2ea59604e8aab83625efb7da3
RockBacon9922 <58570245+RockBacon9922@users.noreply.github.com>
2023-11-08 17:44:06 +0000

    deleted chrome

--- apps/chrome/src/contents/common/utils.ts
+++
@@ -1,9 +0,0 @@
-import { createLot, setActiveLot } from "~slices/auction-slice";
-import { getState, store } from "~store";
-
-import { updateInput } from "@acme/element-operations";
-
-export const setAsk = (askInput: HTMLInputElement, ask: number) => {
-  updateInput(askInput, ask.toString());
-  askInput.click();
-};
--- apps/chrome/src/slices/storageDecoder.js
+++
@@ -1,7 +0,0 @@
-// TypeScript
-const jsonString =
-  '{"auction":"{\\"setup\\":true,\\"started\\":false,\\"paused\\":false,\\"date\\":\\"2023-11-05\\",\\"name\\":\\"sfff\\",\\"currentLotId\\":\\"0\\",\\"lots\\":[]}","platform":"[{\\"name\\":\\"stoneham\\",\\"primary\\":true,\\"status\\":\\"inactive\\"},{\\"name\\":\\"easyliveAuction\\",\\"primary\\":false,\\"status\\":\\"inactive\\"},{\\"name\\":\\"theSaleroom\\",\\"primary\\":false,\\"status\\":\\"inactive\\"}]","_persist":"{\\"version\\":1,\\"rehydrated\\":true}"}';
-// Parse the JSON string
-const jsonObject = JSON.parse(jsonString);
-
-console.log(jsonObject);
