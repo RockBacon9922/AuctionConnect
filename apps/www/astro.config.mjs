9839e2be1e60de2eb192a5bc42437329fd3604cb
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2023-12-13 15:30:03 +0000

    Moved to a static architecture

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   adapter: vercel({
     webAnalytics: {
       enabled: false,
