b9bfd47215941b1d53b0c3cefadc6fb10c10448f
William Stoneham <58570245+RockBacon9922@users.noreply.github.com>
2024-01-06 01:10:38 +0000

    Changed the text on the status bar

--- apps/chrome/src/tabs/Assets/StatusBar.tsx
+++ apps/chrome/src/tabs/Assets/StatusBar.tsx
@@ -1,7 +1,8 @@
 export const StatusBar = () => {
   return (
-    <div className="bottom-0 left-0 w-full bg-gradient-to-b from-abbey-700 to-abbey-800 absolute py-1 px-3 text-abbey-50 text-lg font-bold flex justify-center gap-7">
-      DEV | Gavel Connect
+    <div className="w-full bg-gradient-to-b from-abbey-700 to-abbey-800 py-1 px-3 text-abbey-50 text-lg font-bold flex justify-center gap-7">
+      DEV | Gavel Connect {"{"}In full production this will be the status bar
+      {"}"}
     </div>
   );
 };
